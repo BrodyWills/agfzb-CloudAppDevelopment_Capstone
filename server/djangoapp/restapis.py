ddff0ff7b24c26e4f1ffdac34c71b4ceed8610cf
Brody Wills <brodywills27@gmail.com>
2022-07-14 13:13:09 +0000

    Post Review Backend

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,7 +23,10 @@ def get_request(url, **kwargs):
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, json_payload, **kwargs):
+    response = requests.post(url, params=kwargs, json=json_payload)
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 def get_dealers_from_cf(url, **kwargs):
